3fbd4dfeb7784819ae0f6a069bd84b6ad98ffe27
Chia-Chun Wu <ccwu0918@gmail.com>
2022-05-24 01:28:53 +0800

    Update download_weights.sh
    Google utils file is not there in utils and the file may be modified and renamed to downloads.
    The fix is to change the import from google_utils to downloads
    in download_weights.sh (line 12)
    from utils.google_utils import attempt_download -> from utils.downloads import attempt_download

--- weights/download_weights.sh
+++ weights/download_weights.sh
@@ -4,8 +4,8 @@
 #    $ bash weights/download_weights.sh
 
 python - <<EOF
-from utils.google_utils import attempt_download
-
+#from utils.google_utils import attempt_download
+from utils.downloads import attempt_download
 for x in ['s', 'm', 'l', 'x']:
     attempt_download(f'yolov5{x}.pt')
 
